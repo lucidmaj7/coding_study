66b36a6bd87ce9c507493f17e2f7dd0758cf8445
lucidmaj7 <lucidmaj7@gmail.com>
2019-02-27 13:46:46 +1030

    https://algospot.com/judge/problem/read/URI  Changes to be committed: 	new file:   input 	new file:   urldecode.cpp

---
+++ algospot/URI/urldecode.cpp
@@ -0,0 +1,66 @@
+//https://algospot.com/judge/problem/read/URI
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_LEN 80
+
+
+
+char getSpacialChar(char* en)
+{
+
+	char temp[4]= {0,};
+	memcpy(temp, &en[1],2);
+	
+	return	strtol(temp, NULL,16);
+
+}
+
+void decode(char* enc)
+{
+
+	printf("%s\n",enc);
+	
+	for(int i = 0 ; i< MAX_LEN; i++)
+	{
+		if(enc[i] == '%')
+		{
+			char temp = getSpacialChar(&enc[i]);	
+			if(temp!=0)
+			{
+				enc[i]=temp;
+				
+				printf("%c", enc[i]);
+				i++;
+				i++;
+			}
+		}
+		else
+			printf("%c", enc[i]);
+	}
+
+	return;
+}
+
+
+int main(void)
+{
+	int ncase = 0;
+	char** encodedstr = NULL;
+	scanf("%d\n", &ncase);
+	
+	encodedstr = (char**)malloc(sizeof(char*)*ncase);
+
+
+	for(int i=0;i<ncase;i++)
+	{	
+		encodedstr[i]=(char*)malloc(sizeof(char)*MAX_LEN);
+		memset(encodedstr[i], 0x00, MAX_LEN);
+		scanf("%s\n", encodedstr[i]);
+		decode(encodedstr[i]);
+		printf("\n");
+	}
+	
+
+	return 0;
+}
