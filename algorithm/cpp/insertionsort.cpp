8707365f702e277e57f06fc2d75217575a3969cf
root <root@localhost.localdomain>
2020-10-28 12:47:32 +0900

    삽입정렬 	modified:   insertionsort.cpp

--- algorithm/cpp/insertionsort.cpp
+++ algorithm/cpp/insertionsort.cpp
@@ -1,48 +1,49 @@
 #include <stdio.h>
 
-#define MAX_VAL 9999999
+
 #define ARR_LEN 10
 int arr[ARR_LEN] = {10,23,4,1,6,100,5,3,7,2};
 
-void insertionSort()
+void printArr()
 {
-	for(int i = 0 ;i < ARR_LEN;i++)
-        {
-
-                int curMinIdx  = i;
-                for(int j = i;j<ARR_LEN;j++)
-                {
-                        if(arr[curMinIdx ]>  arr[j])
-                        {
-                                curMinIdx = j;
-                        }
-
-                }
-                int temp = arr[curMinIdx];
-                arr[curMinIdx] =arr[i];
-                arr[i] = temp;
-        }
-
-
-
-}
-
-int main(void)
-{
-
 	for(int i = 0 ;i<ARR_LEN;i++)
 	{
 		printf("%d ", arr[i]);
 	}
 	printf("\n");
 	
+
+}
+
+void insertionSort()
+{
+	for(int i = 1 ;i<ARR_LEN ;i++)
+	{
+
+		int j = i;
+		while(j>=0 && arr[j] < arr[j-1])
+		{
+			
+			int temp = arr[j-1];
+			arr[j-1] = arr[j];
+			arr[j] = temp;
+			j--;
+			printArr();
+		}
+
+		printf("-------\n");	
+	}
+	
+
+}
+
+int main(void)
+{
+	printArr();
+	
 	insertionSort();
 
-	for(int i = 0 ;i<ARR_LEN;i++)
-        {
-                printf("%d ", arr[i]);
-        }
-        printf("\n");
+	printArr();
 
 	return 0;
 }
